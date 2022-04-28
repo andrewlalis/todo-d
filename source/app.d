710379c19f504de4a82fc42a9c28540137b6350b
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-04-28 22:24:47 +0200

    Added resources.

--- source/app.d
+++ source/app.d
@@ -1,16 +1,99 @@
-import std.stdio;
-
 import model;
-import view : setupUI;
+import std.stdio;
 
 import gtk.MainWindow;
 import gtk.Main;
+import gtk.Builder;
+import gtk.Application;
+import gtk.ApplicationWindow;
+import gtk.Entry;
+import gtk.Box;
+import gtk.ListBox;
+import gtk.ListBoxRow;
+import gtk.Label;
+import gtk.CheckButton;
+import gtk.ToggleButton;
+import gtk.Button;
+
+class ToDoItemWidget : Box {
+    this(ToDoItem item, ToDoModel todoModel) {
+        super(GtkOrientation.HORIZONTAL, 5);
+        CheckButton button = new CheckButton();
+        button.setActive(item.checked);
+        button.addOnToggled(delegate(ToggleButton b) {
+			item.checked = b.getActive();
+		});
+        Label label = new Label(item.text);
+        label.setLineWrap(true);
+        label.setHalign(GtkAlign.START);
+        label.setValign(GtkAlign.CENTER);
+        this.packStart(button, false, false, 0);
+        
+        Button removeButton = new Button(StockID.REMOVE);
+        removeButton.addOnClicked(delegate(Button b) {
+			todoModel.removeItem(item.priority);
+        });
+		this.packEnd(removeButton, false, false, 0);
+
+		if (todoModel.canIncrement(item)) {
+			Button upButton = new Button(StockID.GO_UP, delegate(Button b) {
+				todoModel.incrementPriority(item);
+			});
+			this.packEnd(upButton, false, false, 0);
+		}
+		if (todoModel.canDecrement(item)) {
+			Button downButton = new Button(StockID.GO_DOWN, delegate(Button b) {
+				todoModel.decrementPriority(item);
+			});
+			this.packEnd(downButton, false, false, 0);
+		}
+        
+        this.packEnd(label, true, true, 0);
+    }
+}
+
+Entry taskEntry;
+ListBox taskList;
+
+ToDoModel todoModel;
 
 void main(string[] args) {
 	Main.init(args);
-	MainWindow window = new MainWindow("To-Do");
-	window.setDefaultSize(300, 500);
-	setupUI(window);
+	Builder builder = new Builder("todo-ui.glade");
+	
+	builder.addCallbackSymbol("onAddTask", &addTask);
+	builder.addCallbackSymbol("onWindowDestroy", &onWindowDestroy);
+	builder.addCallbackSymbol("onQuitMenuActivated", &onWindowDestroy);
+	builder.connectSignals(null);
+
+	taskList = cast(ListBox) builder.getObject("taskList");
+	taskEntry = cast(Entry) builder.getObject("addTaskEntry");
+
+	todoModel = new ToDoModel();
+	import std.functional : toDelegate;
+	todoModel.addListener(ModelUpdateListener.of(toDelegate(&itemsUpdated)));
+
+	ApplicationWindow window = cast(ApplicationWindow) builder.getObject("window");
 	window.showAll();
 	Main.run();
+}
+
+void itemsUpdated(ToDoItem[] items) {
+	taskList.removeAll();
+	foreach (item; items) {
+		auto widget = new ToDoItemWidget(item, todoModel);
+		auto row = new ListBoxRow();
+		row.add(widget);
+		taskList.add(row);
+	}
+	taskList.showAll();
+}
+
+extern (C) void addTask() {
+	todoModel.addItem(taskEntry.getText());
+	taskEntry.setText("");
+}
+
+extern (C) void onWindowDestroy() {
+	Main.quit();
 }
--- source/model.d
+++ source/model.d
@@ -1,6 +1,92 @@
 module model;
 
-struct ToDoItem {
-    string text;
-    int priority;
+class ToDoItem {
+	string text;
+	int priority;
+	bool checked;
+	this(string text, int priority, bool checked) {
+		this.text = text;
+		this.priority = priority;
+		this.checked = checked;
+	}
+}
+
+interface ModelUpdateListener {
+    void itemsUpdated(ToDoItem[] items);
+
+    static ModelUpdateListener of(void delegate(ToDoItem[]) dg) {
+        return new class ModelUpdateListener {
+            void itemsUpdated(ToDoItem[] items) {
+                dg(items);
+            }
+        };
+    }
+}
+
+class ToDoModel {
+    private ToDoItem[] items;
+    private ModelUpdateListener[] listeners;
+
+    void addListener(ModelUpdateListener listener) {
+        listeners ~= listener;
+    }
+
+    void addItem(string text) {
+        addItem(new ToDoItem(text, 1_000_000, false));
+    }
+
+    void addItem(ToDoItem item) {
+        items ~= item;
+        normalizePrio();
+        notifyListeners();
+    }
+
+    void removeItem(int prio) {
+        import std.algorithm;
+        size_t idx = prio - 1; // Assume prio is normalized to start at 1.
+        items = items.remove(idx);
+        normalizePrio();
+        notifyListeners();
+    }
+
+    bool canIncrement(ToDoItem item) {
+        return item.priority > 1;
+    }
+
+    void incrementPriority(ToDoItem item) {
+        size_t idx = item.priority - 1;
+        if (idx == 0) return;
+        ToDoItem higher = items[idx - 1];
+        item.priority -= 1;
+        higher.priority += 1;
+        items[idx - 1] = item;
+        items[idx] = higher;
+        notifyListeners();
+    }
+
+    bool canDecrement(ToDoItem item) {
+        return item.priority < items.length;
+    }
+
+    void decrementPriority(ToDoItem item) {
+        size_t idx = item.priority - 1;
+        if (idx + 1 >= items.length) return;
+        ToDoItem lower = items[idx + 1];
+        item.priority += 1;
+        lower.priority -= 1;
+        items[idx + 1] = item;
+        items[idx] = lower;
+        notifyListeners();
+    }
+
+    private void normalizePrio() {
+        int prio = 1;
+        foreach (item; items) {
+            item.priority = prio++;
+        }
+    }
+
+    private void notifyListeners() {
+        foreach (l; listeners) l.itemsUpdated(this.items);
+    }
 }
